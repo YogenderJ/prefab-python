f29d355dc1572397e0ebe2b3d8ab891a06c232ee
Yogender Singh Jodha <yogender.jodha@magicedtech.com>
2022-10-11 18:27:23 +0530

    Start python file uploaded

--- Scripts/Install.sh
+++ Scripts/Install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/codepipeline/python
-source env/bin/activate
-python3 app.py > /dev/null 2>&1 &
-touch aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt
+sh -c './start_python.sh'
+#source env/bin/activate
+#python3 app.py > /dev/null 2>&1 &
+#touch aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt
---
+++ start_python.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ubuntu/codepipeline/python
+source env/bin/activate
+nohup python3 app.py > /dev/null 2>&1 &
