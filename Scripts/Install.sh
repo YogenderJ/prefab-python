fbb3eabcf1f1b0d48e1a766aec4c622de73ab8ab
Yogender Singh Jodha <yogender.jodha@magicedtech.com>
2022-10-11 18:02:33 +0530

    Codedeploy Scripts updated

--- Scripts/Install.sh
+++ Scripts/Install.sh
@@ -2,4 +2,4 @@
 cd /home/ubuntu/codepipeline/python
 source env/bin/activate
 nohup python3 app.py > /dev/null 2>&1 &
-touch aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt
+touch aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt
