b8f3fbcb0f76cc344ae64c63c143ca5141b11dfe
Yogender Singh Jodha <yogender.jodha@magicedtech.com>
2022-10-11 18:14:04 +0530

    New Install.sh file

--- Scripts/BeforeInstall.sh
+++ Scripts/BeforeInstall.sh
@@ -2,6 +2,3 @@
 cd /home/ubuntu/codepipeline/python
 for pid in $(lsof -t -i:3000); do kill -9 $pid; done
 sudo rm -rf *
-cd /home/ubuntu/codepipeline/python
-source env/bin/activate
-nohup python3 app.py > /dev/null 2>&1 &
--- Scripts/Install.sh
+++ Scripts/Install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/codepipeline/python
 source env/bin/activate
-nohup python3 app.py > /dev/null 2>&1 &
+python3 app.py > /dev/null 2>&1 &
 touch aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt
