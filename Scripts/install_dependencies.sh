373601eaba95abb85d449151b68e8c36d60e4daa
Yogender Singh Jodha <yogender.jodha@magicedtech.com>
2022-10-12 11:29:47 +0530

    Python Gunicorn added

--- Scripts/install_dependencies.sh
+++ Scripts/install_dependencies.sh
@@ -6,9 +6,12 @@ y=$(echo $x | awk 'NR==1 {print $0}')
 if [[ "$y" =~ .*"Ubuntu".* ]]; then
         sudo apt update -y
         sudo apt install python3.8 -y
+	sudo pip install gunicorn
 elif [[ "$y" =~ .*"Red Hat".* ]] || [[ "$y" =~ .*"Amazon".* ]]; then
 	sudo yum update -y
         sudo yum install -y python3.8
+	sudo pip install gunicorn
+	
 else
         echo "Any other OS"
 fi
--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 cd /home/ubuntu/codepipeline/python/
+nohup gunicorn --bind 0.0.0.0:3000 wsgi:app > /dev/null 2>&1 &
 #sudo sh -c './start_python.sh'
 #source env/bin/activate
 #/usr/bin/python3 app.py > /dev/null 2>&1 &
 #touch aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt
 
 ##Copy custom_created systemd file
-sudo chmod +x my_python.service
-sudo cp my_python.service /etc/systemd/system/
+#sudo chmod +x my_python.service
+#sudo cp my_python.service /etc/systemd/system/
 
 ##add exceutable permissions to go app
-sudo chmod +x /home/ubuntu/codepipeline/python/start_python.sh
+#sudo chmod +x /home/ubuntu/codepipeline/python/start_python.sh
 
 ##Allows any users to write the app folder. Useful if using fs within the app
-sudo chmod go+w /home/ubuntu/codepipeline/python
+#sudo chmod go+w /home/ubuntu/codepipeline/python
 
 ##Launches the express app
-sudo systemctl daemon-reload
-sudo systemctl start my_python.service
-sudo systemctl enable my_python.service
+#sudo systemctl daemon-reload
+#sudo systemctl start my_python.service
+#sudo systemctl enable my_python.service
